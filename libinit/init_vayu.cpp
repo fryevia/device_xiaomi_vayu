147ae241b2c7a23a65f9d6926c2e7de935c75dc7
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-05 05:57:10 +0900

    vayu: libinit: set dalvik heap configuration conditionally
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>

--- libinit/init_vayu.cpp
+++ libinit/init_vayu.cpp
@@ -36,6 +36,7 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
@@ -60,6 +61,27 @@ void property_override(char const prop[], char const value[], bool add = true) {
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void load_dalvik_properties() {
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram < 7000ull * 1024 * 1024) {
+        // 6GB RAM
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heaptargetutilization", "0.5");
+        property_override("dalvik.vm.heapmaxfree", "32m");
+    } else {
+        // 8GB RAM
+        property_override("dalvik.vm.heapstartsize", "24m");
+        property_override("dalvik.vm.heaptargetutilization", "0.46");
+        property_override("dalvik.vm.heapmaxfree", "48m");
+    }
+
+    property_override("dalvik.vm.heapgrowthlimit", "256m");
+    property_override("dalvik.vm.heapsize", "512m");
+    property_override("dalvik.vm.heapminfree", "8m");
 }
 
 void set_device_props(const std::string fingerprint, const std::string description,
@@ -121,6 +143,8 @@ void vendor_load_properties() {
         property_override("ro.product.mod_device", "vayu_global");
     }
 
+    load_dalvik_properties();
+    
 //  SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
     property_override("ro.oem_unlock_supported", "0");
