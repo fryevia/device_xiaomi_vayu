1df727d2093e10b4febbce77a451e61382ec1001
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    vayu: Remove some Gapps packages at build time
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>

--- device.mk
+++ device.mk
@@ -385,6 +385,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
