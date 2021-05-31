1330a1851df48f34857ce35844a8d482537b2a6f
Foxtrot47 <jjneutron@outlook.com>
2021-05-31 09:55:32 +0000

    vayu: compile kernel with proton clang
    * Also add some additional kernel flags
    
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,10 +60,11 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_CONFIG := vayu_user_defconfig
+  TARGET_KERNEL_CONFIG := vayu_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
-  TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+  TARGET_KERNEL_CLANG_VERSION := proton
+  TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument" AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld
 endif
 
 # Partitions
