ae785f7ea62b9052c5a79136bdd16b18b6648856
Nauval Rizky <enuma.alrizky@gmail.com>
2021-06-21 10:19:21 +0700

    vayu: parts: Introduce haptic feedback level adjustment
    Since our kernel already expose vmax_mv as adjustable level, lets add
    a simple UI for it.
    
    Co-authored-by: David Setiawan <valkry24@gmail.com>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,6 +24,7 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.haptic.HapticUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -41,5 +42,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
+        HapticUtils.restoreLevel(context);
     }
 }
