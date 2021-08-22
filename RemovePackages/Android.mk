7d3739a70a7a9db7d2a24d99c8cecd1c13e890b5
David Setiawan <fryevia@foxmail.com>
2021-08-23 03:06:54 +0700

    vayu: RemovePackages: Rearrange LOCAL_OVERRIDES_PACKAGES
    Signed-off-by: David Setiawan <fryevia@foxmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,35 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Chrome Chrome-Stub DevicePolicyPrebuilt Drive GoogleCamera Maps MyVerizonServices OPScreenRecord Ornament OBDM_Permissions OemDmTrigger PixelWallpapers2020 PixelLiveWallpaperPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt Snap YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger WallpapersBReel2020 obdm_stub
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    Chrome \
+    Chrome-Stub \
+    DevicePolicyPrebuilt \
+    Drive \
+    GoogleCamera \
+    Maps \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PixelLiveWallpaperPrebuilt \
+    PixelWallpapers2020 \
+    SafetyHubPrebuilt \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    WallpapersBReel2020 \
+    YouTube \
+    YouTubeMusicPrebuilt
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
